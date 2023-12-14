d70ca75243f5bacd6e6834bf0a2c9e553ae95a64
Rasuljka <ras199167@gmail.com>
2023-12-14 19:47:06 +0300

    fix

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+item3 = Item("Ноутбук", 5000, 2)
+assert item3.name == "Ноутбук"
+assert item3.price == 5000
+assert item3.quantity == 2
+assert item3.calculate_total_price() == 10000

89f1fdaf8ab693e85a4655a7880028cc186e60c9
Rasuljka <ras199167@gmail.com>
2023-12-18 16:54:04 +0300

    fix

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
 
 from src.item import Item
 
-item3 = Item("Ноутбук", 5000, 2)
-assert item3.name == "Ноутбук"
-assert item3.price == 5000
-assert item3.quantity == 2
-assert item3.calculate_total_price() == 10000
+
+@pytest.fixture
+def test___init__():
+    item3 = Item("Ноутбук", 5000, 2)
+    assert item3.name == "Ноутбук"
+    assert item3.price == 5000
+    assert item3.quantity == 2
+
+
+def test_calculate_total_price():
+    item3 = Item("Ноутбук", 5000, 2)
+    assert item3.calculate_total_price() == 10000
+
+
+def test_apply_discount():
+    item3 = Item("Ноутбук", 5000, 2)
+    assert item3.apply_discount() is None
